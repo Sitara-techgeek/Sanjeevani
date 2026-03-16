1b9f7919da3979c3510ab632bd1c493af547c0a9
K.Sravani <k.sravani3274@gmail.com>
2026-03-17 04:46:17 +0530

    Add CORS origin for Sanjeevani frontend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,8 @@ app = FastAPI(title="AYUSETU API", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://sanjeevani-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
